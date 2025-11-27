81627fc40a1daa4ee6b6bd26a99dd2d332e12b03
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-27 16:24:19 +0700

    feat: Implement ContainerDataApp to provide repository dependencies.

--- app/src/main/java/com/example/prak7_roomdatabase/repository/ContainerApp.kt
+++ app/src/main/java/com/example/prak7_roomdatabase/repository/ContainerApp.kt
@@ -1,6 +1,18 @@
 package com.example.prak7_roomdatabase.repository
 
+import android.content.Context
+import com.example.prak7_roomdatabase.room.DatabaseSiswa
+
 interface ContainerApp {
     val repositoriSiswa: RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp {
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
 }
 
