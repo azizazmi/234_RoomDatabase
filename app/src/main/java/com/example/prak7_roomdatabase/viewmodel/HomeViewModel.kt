3b468f4e25c88c232cb9f8743d099c329828ce1a
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-27 17:01:30 +0700

    feat: Add TIMEOUT_MILLIS constant to HomeViewModel

--- app/src/main/java/com/example/prak7_roomdatabase/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/prak7_roomdatabase/viewmodel/HomeViewModel.kt
@@ -4,5 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.example.prak7_roomdatabase.repository.RepositoriSiswa
 
 class HomeViewModel (private val repositoriSiswa: RepositoriSiswa): ViewModel() {
-
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
 }
