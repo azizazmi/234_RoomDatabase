33241160a2cf0170ef1c8b846df5377b3bdd2845
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-11-27 17:40:56 +0700

    feat: Add `PenyediaViewModel` to provide a ViewModel Factory

---
+++ app/src/main/java/com/example/prak7_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.prak7_roomdatabase.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.prak7_roomdatabase.repository.AplikasiSiswa
+import com.example.prak7_roomdatabase.viewmodel.EntryViewModel
+import com.example.prak7_roomdatabase.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
