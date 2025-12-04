445e93dc0d86be6840a071dddb025db09c5a4721
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-12-04 14:37:08 +0700

    feat: Add `DetailViewModel` and `EditViewModel` to `PenyediaViewModel`

--- app/src/main/java/com/example/prak7_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak7_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.prak7_roomdatabase.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.prak7_roomdatabase.repository.AplikasiSiswa
+import com.example.prak7_roomdatabase.viewmodel.DetailViewModel
+import com.example.prak7_roomdatabase.viewmodel.EditViewModel
 import com.example.prak7_roomdatabase.viewmodel.EntryViewModel
 import com.example.prak7_roomdatabase.viewmodel.HomeViewModel
 
@@ -15,6 +18,15 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+        initializer {
+            EditViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
