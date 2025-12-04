f529dd30cc1e7b47a10024f6251d422e38a13d3e
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2025-12-04 12:59:49 +0700

    feat: Add `DestinasiHome` object for navigation routing.

---
+++ app/src/main/java/com/example/prak7_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,11 @@
+package com.example.prak7_roomdatabase.view.route
+
+import com.example.prak7_roomdatabase.R
+
+object DestinasiHome {
+    override val route = "home"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+
+}
